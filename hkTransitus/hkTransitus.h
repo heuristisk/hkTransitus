2fbb27174d65f9e55353d13cfb30c3f8b34ff2d3
Anderson Santos Gusmao <developer@MacBook-Pro-de-Anderson-2.local>
2017-12-02 19:28:31 -0200

    Initial Commit

---
+++ hkTransitus/hkTransitus.h
@@ -0,0 +1,19 @@
+//
+//  hkTransitus.h
+//  hkTransitus
+//
+//  Created by Anderson Santos Gusmão on 02/12/2017.
+//  Copyright © 2017 Anderson Santos Gusmão. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for hkTransitus.
+FOUNDATION_EXPORT double hkTransitusVersionNumber;
+
+//! Project version string for hkTransitus.
+FOUNDATION_EXPORT const unsigned char hkTransitusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <hkTransitus/PublicHeader.h>
+
+
